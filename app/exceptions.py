d135cef85d8ffdac5e752338fd47e65bc4ba1cd0
Lymar Volodymyr <shaxtar11@gmail.com>
2024-05-31 10:00:07 +0200

    Added room booking function

--- app/exceptions.py
+++ app/exceptions.py
@@ -2,7 +2,7 @@ from fastapi import HTTPException, status
 
 
 class BookingException(HTTPException):
-    status_code = 500
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     detail = ""
 
     def __init__(self):
@@ -10,29 +10,34 @@ class BookingException(HTTPException):
 
 
 class UserAlreadyExistsException(BookingException):
-    status_code = status.HTTP_409_CONFLICT,
+    status_code = status.HTTP_409_CONFLICT
     detail = "User already exists"
 
 
 class IncorrectEmailOrPasswordException(BookingException):
-    status_code = status.HTTP_401_UNAUTHORIZED,
+    status_code = status.HTTP_401_UNAUTHORIZED
     detail = "Invalid email or password"
 
 
 class TokenExpiredException(BookingException):
-    status_code = status.HTTP_401_UNAUTHORIZED,
+    status_code = status.HTTP_401_UNAUTHORIZED
     detail = "Token has expired"
 
 
 class TokenAbsentException(BookingException):
-    status_code = status.HTTP_401_UNAUTHORIZED,
+    status_code = status.HTTP_401_UNAUTHORIZED
     detail = "The token is missing"
 
 
 class IncorrectTokenFormatException(BookingException):
-    status_code = status.HTTP_401_UNAUTHORIZED,
+    status_code = status.HTTP_401_UNAUTHORIZED
     detail = "Incorrect token format"
 
 
 class UserIsNotPresentException(BookingException):
     status_code = status.HTTP_401_UNAUTHORIZED
+
+
+class RoomCanNotBeBooked(BookingException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = "No rooms left"
